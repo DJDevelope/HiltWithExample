52dd42f9bc69a44fb17283f078496fdf0f511334
Dhananjay Singh <dhananjaysingh271@gmail.com>
2025-03-29 10:10:51 +0530

    Added dependecy successfully...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.kotlin.kapt) // Correctly apply the kapt plugin using alias
 }
 
 android {
@@ -56,4 +58,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+
+    // Hilt Dependencies
+    implementation(libs.hilt.android)  // Hilt Android Dependency
+    kapt(libs.hilt.compiler)  // Hilt compiler for annotation processing
+
 }
